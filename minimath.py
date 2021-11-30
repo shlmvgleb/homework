deb0773fd609e992fe27ea2e84e208f5ba39fb34
shlmvgleb <90252107+shlmvgleb@users.noreply.github.com>
2021-11-30 21:13:24 +0300

    Add files via upload

---
+++ minimath.py
@@ -0,0 +1,27 @@
+from math import sqrt
+
+
+class MyMath:
+    @staticmethod
+    def addition(a, b):
+        return a + b
+
+    @staticmethod
+    def multiplication(a, b):
+        return a * b
+
+    @staticmethod
+    def division(a, b):
+        return a / b
+
+    @staticmethod
+    def substraction(a, b):
+        return a - b
+
+    @staticmethod
+    def pow(a, b):
+        return a ** b
+
+    @staticmethod
+    def sqrt(a):
+        return sqrt(a)
